0deb5064b1e56ca9868635d125380c5d0d7676d7
Melkor-1 <140725427+Melkor-1@users.noreply.github.com>
2023-10-19 12:35:09 -0700

    Update passwd_checker.c

--- passwd_checker.c
+++ passwd_checker.c
@@ -10,23 +10,24 @@
 #define HAS_DIGIT   0x04    /* 0b00000100 */
 #define HAS_SPECIAL 0x08    /* 0b00001000 */
 
-bool strongPasswordCheckerII(const char *password)
+static bool strongPasswordCheckerII(const char *password)
 {
-    size_t const sz = strlen (password);
+    size_t const sz = strlen(password);
     if (sz < 8) {
         return false;
     }
 
     unsigned char flags = 0;
+	
     for (size_t i = 0; i < sz; ++i) {
         if (i > 0 && password[i - 1] == password[i]) {
             return false;
         }
     
         unsigned char ch = password[i];
-        flags |= islower (ch) ? HAS_LOWER :
-                 isupper (ch) ? HAS_UPPER :
-                 isdigit (ch) ? HAS_DIGIT :
+        flags |= islower(ch) ? HAS_LOWER :
+                 isupper(ch) ? HAS_UPPER :
+                 isdigit(ch) ? HAS_DIGIT :
                  HAS_SPECIAL;
 
 	if ((flags & HAS_LOWER) && (flags & HAS_UPPER) && (flags & HAS_DIGIT) && (flags & HAS_SPECIAL)) {
@@ -38,9 +39,9 @@ bool strongPasswordCheckerII(const char *password)
 
 int main(void) 
 {
-	assert (strongPasswordCheckerII ("IloveLe3tcode$"));
-	assert (!strongPasswordCheckerII ("Me+You--IsMyDream"));
-	assert (!strongPasswordCheckerII ("1aB!"));
+	assert(strongPasswordCheckerII ("IloveLe3tcode$"));
+	assert(!strongPasswordCheckerII ("Me+You--IsMyDream"));
+	assert(!strongPasswordCheckerII ("1aB!"));
 	return EXIT_SUCCESS;
 }
 
